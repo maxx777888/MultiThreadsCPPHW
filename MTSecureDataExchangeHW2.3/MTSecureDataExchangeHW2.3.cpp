d25f8d44f7b584c3d05b1961e19c4d90260b94da
Andrei Fridman <judoostudy@mail.ru>
2023-07-26 20:47:55 +0300

    HW2.3 changed

--- MTSecureDataExchangeHW2.3/MTSecureDataExchangeHW2.3.cpp
+++ MTSecureDataExchangeHW2.3/MTSecureDataExchangeHW2.3.cpp
@@ -7,7 +7,19 @@ class Data {
 public:
     int num1;
     int num2;
-    std::mutex mtx;
+    mutable std::mutex mtx;
+
+    Data& operator=(const Data& other)
+    {
+        if (this != &other) {
+
+            std::lock_guard<std::mutex> lock(other.mtx);
+            this->num1 = other.num1;
+            this->num2 = other.num2;
+        }
+        
+        return *this;
+    }
 
     static void print_data(const Data& some_data) {
         std::cout << some_data.num1 << " " << some_data.num2 << std::endl;
@@ -17,43 +29,26 @@ public:
 void swap_lock(Data& data1, Data& data2) {
 
     std::lock(data1.mtx, data2.mtx);
-
     std::lock_guard<std::mutex> lock1(data1.mtx, std::adopt_lock);
-    int temp = data1.num1;
-    data1.num1 = data2.num1;
-    data2.num1 = temp;
-
     std::lock_guard<std::mutex> lock2(data2.mtx, std::adopt_lock);
-    temp = data2.num2;
-    data2.num2 = data1.num2;
-    data1.num2 = temp;
+    std::swap(data1.num1, data2.num1);
+    std::swap(data1.num2, data2.num2);
 }
 void swap_unique_lock(Data& data1, Data& data2) {
 
     std::unique_lock<std::mutex> lock1(data1.mtx, std::defer_lock);
-    int temp = data1.num1;
-    data1.num1 = data2.num1;
-    data2.num1 = temp;
-
-
     std::unique_lock<std::mutex> lock2(data2.mtx, std::defer_lock);
-    temp = data2.num2;
-    data2.num2 = data1.num2;
-    data1.num2 = temp;
-
+    std::swap(data1.num1, data2.num1);
+    std::swap(data1.num2, data2.num2);
     std::lock(lock1, lock2);
 }
 
 void swap_scoped_lock(Data& data1, Data& data2) {
     std::scoped_lock(data1.mtx, data2.mtx);
 
-    int temp = data1.num1;
-    data1.num1 = data2.num1;
-    data2.num1 = temp;
+    std::swap(data1.num1, data2.num1);
+    std::swap(data1.num2, data2.num2);
 
-    temp = data2.num2;
-    data2.num2 = data1.num2;
-    data1.num2 = temp;
 }
 
 
