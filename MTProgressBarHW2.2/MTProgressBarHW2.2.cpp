a9e7cef7db301b7bd8979779f577a2be106f345a
Andrei Fridman <judoostudy@mail.ru>
2023-07-26 16:33:26 +0300

    remarks num_1

--- MTProgressBarHW2.2/MTProgressBarHW2.2.cpp
+++ MTProgressBarHW2.2/MTProgressBarHW2.2.cpp
@@ -7,7 +7,6 @@
 
 std::mutex mtx;
 
-
 //Метод устанавливает курсор в консоли на указанную позицию
 void put_cur(int x, int y)
 {
@@ -17,21 +16,30 @@ void put_cur(int x, int y)
         });
 }
 
-void progress_bar(int x, int y, int w, double progress, char s_bar) {
+
+void progress_bar(int x, int y, int w, double progress, char s_bar, bool flag, int& start_point) {
+
     int fill_w = static_cast<int> (w * progress);
     int empty_w = w - fill_w;
-    put_cur(x, y);
-    for (int i = 0; i < fill_w; i++) {
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), y);
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    put_cur(x + start_point, y);
+    for (int i = start_point; i < fill_w; i++) {
+        //SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), y);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         std::cout << s_bar;
     }
+    if (flag) {
+        for (int j = 0; j < empty_w - 1; j++) {
+            std::cout << ".";
+        }
+        flag = false;
+    }
 
-    for (int i = 0; i < empty_w; i++)
-        std::cout << " ";
+    start_point = fill_w;
 }
 
 void print_progress_bar(int id, int n_thread, int bar_length) {
+
     int prog_x = 7;
     int prog_y = id;
     int b_width = bar_length;
@@ -40,14 +48,22 @@ void print_progress_bar(int id, int n_thread, int bar_length) {
 
     auto start = std::chrono::steady_clock::now();
     double speed = 0.03;
+    bool flag = true;
+    int bar_start = 0;
     while (bar < 1) {
-        std::unique_lock ul(mtx);
+
+        mtx.lock();
         put_cur(1, prog_y);
         std::cout << " " << ((bar + speed >= 1) ? 1 * 100 : bar * 100) << "% ";
-        progress_bar(prog_x, prog_y, b_width, bar, '#');
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-        std::cout << " Поток: " << id << " Идентификатор потока: " << std::this_thread::get_id() << std::endl;
+        progress_bar(prog_x, prog_y, b_width, bar, '#', flag, bar_start);
+        //SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+        put_cur(prog_x + b_width, prog_y);
+        std::cout << " Поток: " << id;
+        put_cur(prog_x + b_width + 11, prog_y);
+        std::cout << " Идентификатор потока: " << std::this_thread::get_id() << std::endl;
+        mtx.unlock();
         bar += speed;
+
     }
     std::lock_guard lg(mtx);
     auto end = std::chrono::steady_clock::now();
--- MTSecureDataExchangeHW2.3/MTSecureDataExchangeHW2.3.cpp
+++ MTSecureDataExchangeHW2.3/MTSecureDataExchangeHW2.3.cpp
@@ -2,77 +2,93 @@
 #include <mutex>
 
 
+
 class Data {
 public:
-
-    Data(int data_) { this->data = data_;}
-
-    Data() { this->data = 0; }
-
-    void swap_lock(Data& data_x, Data& data_y) 
-    {
-        mut.lock();
-        int temp = data_x.data;
-        data_x.data = data_y.data;
-        data_y.data = temp;
-        mut.unlock();
-    }
-    void swap_unique_lock(Data& data_x, Data& data_y)
-    {
-        std::unique_lock ul(mut);
-        int temp = data_x.data;
-        data_x.data= data_y.data;
-        data_y.data = temp;
-        ul.unlock();
-    }
-
-   void swap_scope_lock(Data& data_x, Data& data_y)
-    {
-        std::scoped_lock sl(mut);
-        int temp = data_x.data;
-        data_x.data = data_y.data;
-        data_y.data = temp;
-        
-    }
+    int num1;
+    int num2;
+    std::mutex mtx;
 
     static void print_data(const Data& some_data) {
-        std::cout << some_data.data << std::endl;
+        std::cout << some_data.num1 << " " << some_data.num2 << std::endl;
     }
-
-private:
-    int data;
-    std::mutex mut;
 };
 
+void swap_lock(Data& data1, Data& data2) {
+
+    std::lock(data1.mtx, data2.mtx);
+
+    std::lock_guard<std::mutex> lock1(data1.mtx, std::adopt_lock);
+    int temp = data1.num1;
+    data1.num1 = data2.num1;
+    data2.num1 = temp;
+
+    std::lock_guard<std::mutex> lock2(data2.mtx, std::adopt_lock);
+    temp = data2.num2;
+    data2.num2 = data1.num2;
+    data1.num2 = temp;
+}
+void swap_unique_lock(Data& data1, Data& data2) {
+
+    std::unique_lock<std::mutex> lock1(data1.mtx, std::defer_lock);
+    int temp = data1.num1;
+    data1.num1 = data2.num1;
+    data2.num1 = temp;
+
+
+    std::unique_lock<std::mutex> lock2(data2.mtx, std::defer_lock);
+    temp = data2.num2;
+    data2.num2 = data1.num2;
+    data1.num2 = temp;
+
+    std::lock(lock1, lock2);
+}
+
+void swap_scoped_lock(Data& data1, Data& data2) {
+    std::scoped_lock(data1.mtx, data2.mtx);
+
+    int temp = data1.num1;
+    data1.num1 = data2.num1;
+    data2.num1 = temp;
+
+    temp = data2.num2;
+    data2.num2 = data1.num2;
+    data1.num2 = temp;
+}
+
+
 int main() {
-    
-    Data data1(20), data2(55), obj;
+
+    Data data1(20, 10), data2(50, 40);
     std::cout << "=========== Starting data ===============" << std::endl;
     std::cout << "Data1 - > ";
     Data::print_data(data1);
     std::cout << "Data2 - > ";
     Data::print_data(data2);
     std::cout << "=========================================" << std::endl;
-    std::cout << "Swapping two objects using lock() - >" << std::endl;
-    obj.swap_lock(data1, data2);
+
+    std::cout << "Swapping using lock() - >" << std::endl;
+    swap_lock(data1, data2);
+    std::cout << "Data1 - > ";
+    Data::print_data(data1);
+    std::cout << "Data2 - > ";
+    Data::print_data(data2);
+    std::cout << "=========================================" << std::endl;
+
+    std::cout << "Swapping using unique_lock() - >" << std::endl;
+    swap_unique_lock(data1, data2);
+    std::cout << "Data1 - > ";
+    Data::print_data(data1);
+    std::cout << "Data2 - > ";
+    Data::print_data(data2);
+    std::cout << "=========================================" << std::endl;
+
+    std::cout << "Swapping using scoped_lock() - >" << std::endl;
+    swap_scoped_lock(data1, data2);
     std::cout << "Data1 - > ";
     Data::print_data(data1);
     std::cout << "Data2 - > ";
     Data::print_data(data2);
 
-    std::cout << "Swapping two objects using unique_lock() - >" << std::endl;
-    obj.swap_unique_lock(data1, data2);
-    std::cout << "Data1 - > ";
-    Data::print_data(data1);
-    std::cout << "Data2 - > ";
-    Data::print_data(data2);
-
-    std::cout << "Swapping two objects using scoped_lock() - >" << std::endl;
-    obj.swap_unique_lock(data1, data2);
-    std::cout << "Data1 - > ";
-    Data::print_data(data1);
-    std::cout << "Data2 - > ";
-    Data::print_data(data2);
-    
     return 0;
 }
