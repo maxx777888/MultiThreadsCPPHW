d600e622891bfabcca467a9b2d5b67b5d56259e2
Andrei Fridman <judoostudy@mail.ru>
2023-07-29 18:22:37 +0300

    Async HW added

---
+++ MTForEachHW3.2/MTForEachHW3.2.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <random>
+#include <vector>
+#include <future>
+
+
+
+//Создаем вектор и заполняем его случайными числами
+std::vector<int> create_vector(int size) {
+    std::vector<int> vec(size);
+    int r = (rand() % 100) + 1;
+    std::srand(std::time(nullptr) + r);
+    for (int i = 0; i < size; ++i) {
+        vec[i] = (rand() % 100) + 1;
+    }
+    return vec;
+}
+//Метод проверяет ячейку вектора и если число четное выводит на консоль
+void print_even_number(int i) 
+{
+    if (i % 2 == 0) 
+    {
+        std::cout << i << "\t";
+    }
+}
+
+
+template<typename It, typename Func>
+void for_each_async(It begin, It end, Func func) 
+{
+    auto cur_size = std::distance(begin, end);
+    if (cur_size <= 5) {
+        
+        std::for_each(begin, end, func);
+        return;
+    }
+    auto mid = begin;
+    std::advance(mid, cur_size / 2);
+    
+    auto ft_res = std::async(for_each_async<It, Func>, begin, mid, func);
+    for_each_async(begin, mid, func);
+
+}
+
+int main()
+{
+    int vec_size = 10;
+    std::vector<int> vec = create_vector(vec_size);
+
+    //Проверка на работу с лямбдо функцией 
+    for_each_async(vec.begin(), vec.end(), [](int i) {
+        std::cout << i << "\t";
+        });
+
+    std::cout << std::endl;
+    std::cout << "Output of even numbers in a vector: " << std::endl;
+    //Проверка на работу с функцией
+    for_each_async(vec.begin(), vec.end(), print_even_number);
+
+}
+
---
+++ MTSortingByChoiceHW3.1/MTSortingByChoiceHW3.1.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <future>
+
+
+//Создаем вектор и заполняем его случайными числами
+std::vector<int> create_vector(int size) {
+    std::vector<int> vec(size);
+    int r = (rand() % 100) + 1;
+    std::srand(std::time(nullptr) + r);
+    for (int i = 0; i < size; ++i) {
+        vec[i] = (rand() % 100) + 1;
+    }
+    return vec;
+}
+
+//Метод выводит содержимое вектора на консоль
+void print_vec(const std::vector<int>& vector) {
+    for (auto& v : vector) {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+}
+//Метод сортирует вектор с наименьшего числа к наибольшему
+void sort_by_choice(std::vector<int>& v, std::promise<std::vector<int>> prom)
+{    
+    for (int i = 0; i < v.size() - 1; i++) 
+    {
+        int min_index = i;
+        int min_num = v[i];
+        bool min_found{ false };
+
+        for (int j = i + 1; j < v.size(); j++) 
+        {               
+            if (min_num > v[j])
+            {
+                min_index = j;
+                min_num = v[j];
+                min_found = true;
+            }
+        }
+        if (min_found) {
+            std::swap(v[i], v[min_index]);
+        }
+    }
+    prom.set_value(v);
+}
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int vec_size = 20;
+    std::vector<int> vec = create_vector(vec_size);
+    std::cout << "Start order: -> ";
+    print_vec(vec);
+
+    std::promise<std::vector<int>> prom;
+    auto ft_res = prom.get_future();
+    auto ft = std::async(sort_by_choice, std::ref(vec), std::move(prom));
+    
+    ft.get();
+
+  
+    std::cout << "New order: -> ";
+    print_vec(vec);
+
+}
