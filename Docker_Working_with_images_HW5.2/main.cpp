2a3f945d5942c24fee87268c6477b27d81fcdf05
Andrei Fridman <judoostudy@mail.ru>
2023-08-05 23:50:47 +0300

    Docker HW added

---
+++ Docker_Working_with_images_HW5.2/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <sstream>
+
+int main()
+{
+    std::string phone_number = "1234567890";
+    std::stringstream ss;
+    ss << "+7(" << phone_number.substr(0, 3) << ") " << phone_number.substr(3, 3) << "-" << phone_number.substr(6, 2)<< "-" <<phone_number.substr(8) ;
+    std::string formatted_phone_number = ss.str();
+
+    std::cout << "Исходный номер телефона: " << phone_number << std::endl;
+    std::cout << "Форматированный номер телефона: " << formatted_phone_number << std::endl;
+
+    return 0;
+}
---
+++ Installing_Docker_HW5.1/hello_world.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello world! From my PC!!!" << std::endl;
+	return 0;
+}
